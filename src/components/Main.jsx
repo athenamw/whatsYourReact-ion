0843117f0c31a0e5844055e9c1c0b4d52c29c0de
athenamw <wallisathena@gmail.com>
2023-10-05 01:25:07 -0700

    fix spacing and typewriter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,20 +8,23 @@ const Main = () => {
         <section id='main'>
             <img className="w-full h-screen object-cover object-left" src="https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQyfHx0ZXh0dXJlZCUyMGJhY2tncm91bmQlMjBibHVlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=600" alt="lavendar gradient" />            
             <section className="w-full h-screen absolute top-0 left-0 bg-white/30">
-                <section>
-                    <h1>I'm Athena Wallis</h1>
+                <section className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+                    <h1 className="sm:text-5xl text:4xl font-bold text-gray-800">Athena Wallis</h1>
                     
-                    <h2>I'm a  <Typewriter
+                    <h2 className="flex sm:text-3xl text-2-xl pt-4 text-gray-800">
+                 
+                    <Typewriter
                         options={{
-                        strings: ['Developer', 'Customer Service Expert', 'Lifelong Learner'],
+                        strings: [' Developer', ' Customer Service Expert', ' Lifelong Learner', 'Creative Thinker' ],
                         autoStart: true,
                         loop: true,
+                        
                         }}
                      />
                     </h2>
-                    <section>
-                        <FaGithub />
-                        <FaLinkedinIn />
+                    <section className="flex justify-between pt-6 max-w-[200px] w-full">
+                        <FaGithub className="cursor-pointer" size={20}/>
+                        <FaLinkedinIn className="cursor-pointer" size={20} />
                     </section>
                 </section>
 
