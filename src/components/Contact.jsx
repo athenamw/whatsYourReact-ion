d9a5193b7c78f5b155026f0ea19a061cad940d34
athenamw <wallisathena@gmail.com>
2023-10-07 07:30:16 -0700

    form validates add resume file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useState } from "react";
+
 
 const Contact = () => {
+    
+
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
+
+    const validateForm = (event) =>{
+        event.preventDefault();
+        if (name.length == 0) {
+            alert('Name Required')
+            return
+        }
+        if (email.length == 0) {
+            alert('Email Required')
+            return
+        }
+        if (subject.length == 0) {
+            alert('Subject Required')
+            return
+        }
+       
+    }
+
+
+
     return (
         <section id='contact' className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
             <h1 className="py-4 text-4xl font-bold text-center text-violet-800"></h1>
-            <form action="" method="POST" encType="multipart/form-data">
+            <form encType="multipart/form-data">
                 <section className="grid md:grid-cols-2 gap-4 w-full py-2">
                     <section className="flex flex-col">
                         <label className="uppercase text-sm py-2">Name</label>
@@ -12,6 +41,7 @@ const Contact = () => {
                         className="border-2 rounded-lg p-3 flex border-gray-300" 
                         type="text" 
                         name="name"
+                        onChange={(event) => setName(event.target.value)}
                         />                        
                     </section>
                     <section 
@@ -20,28 +50,36 @@ const Contact = () => {
                         <input 
                         className="border-2 rounded-lg p-3 flex border-gray-300" 
                         type="text" 
-                        name="phone"/>                        
+                        name="phone"
+                        onChange={(event) => setPhone(event.target.value)}
+                        />                        
                     </section>
                     <section className="flex flex-col py-2">
                         <label className="uppercase text-sm py-2">Email</label>
                         <input 
                         className="border-2 rounded-lg p-3 flex border-gray-300" 
-                        type="email" 
-                        name="email"/>                        
+                        type="email"
+                        name="email"
+                        onChange={(event) => setEmail(event.target.value)}
+                        />                        
                     </section>
                     <section className="flex flex-col py-2">
                         <label className="uppercase text-sm py-2">Subject</label>
                         <input 
                         className="border-2 rounded-lg p-3 flex border-gray-300" 
                         type="text" 
-                        name="subject"/>                        
+                        name="subject"
+                        onChange={(event) => setSubject(event.target.value)}
+                        />                        
                     </section>
                     <section className="flex flex-col py-2">
                         <label className="uppercase text-sm py-2">Message</label>
-                        <textarea className="border-2 rounded-lg border-gray-300" rows="10" name="message"></textarea>                      
+                        <textarea className="border-2 rounded-lg border-gray-300" rows="10" id="message" name="message" onChange={(event) => setMessage(event.target.value)}></textarea>                      
                     </section>                    
                 </section>
-                <button className="bg-[#6A1B9A] text-gray-100 mt-4 w-full p-4 rounded-lg">
+                <button type="submit" className="cursor-pointer bg-[#6A1B9A] text-gray-100 mt-4 w-full p-4 rounded-lg" onClick={()=> {
+                    validateForm()
+                }}>
                         Send Message
                     </button>
             </form>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,10 +11,10 @@ const Main = () => {
             <img className="w-full h-screen object-cover object-left" src="https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQyfHx0ZXh0dXJlZCUyMGJhY2tncm91bmQlMjBibHVlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=600" alt="lavender gradient" /> 
 
             <section className="w-full h-screen absolute top-0 left-0 bg-white/30 align-items-center">           
-                <section className="max-w-[700px] m-auto pl-10 h-full w-full flex flex-col justify-center lg:items-center items-center">                   
+                <section className="max-w-[700px] m-auto pl-10 h-full w-full flex flex-col justify-center lg:items-center items-center">
+                                   
                     <img src={me} className="rounded-full"/>                 
                     <h1 className="sm:text-5xl text:4xl font-bold text-violet-800">Athena Wallis</h1>                    
-                    
                     <h2 className="flex sm:text-3xl text-2-xl pt-4 text-violet-800">                 
                     <Typewriter
                         options={{
@@ -24,13 +24,13 @@ const Main = () => {
                         }}
                      />
                     </h2>
-                   
-                    <section className="flex justify-between  pt-6 max-w-[200px] w-full fixed bottom-0">
+                                      
+                    <section className="flex p-8 space-x-4 max-w-[200px] w-full fixed bottom-4">
                         <a href="https://github.com/athenamw">
-                        <FaGithub className="cursor-pointer" size={20}/>
+                        <FaGithub className="cursor-pointer" size={30}/>
                         </a>
                         <a href="https://www.linkedin.com/in/athenawallis/">
-                        <FaLinkedinIn className="cursor-pointer" size={20} />
+                        <FaLinkedinIn className="cursor-pointer" size={30} />
                         </a>
                     </section>
                 </section>
